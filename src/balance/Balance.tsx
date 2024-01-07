a604ba756f18f7d873b8c1dfc78ba84005f84dc6
pabloaguilarm <paguilar@heytrade.com>
2024-01-07 20:39:55 +0100

    Getting balance when wallet is connected

--- src/balance/Balance.tsx
+++ src/balance/Balance.tsx
@@ -1,11 +1,24 @@
+import {useConnection, useWallet} from "@solana/wallet-adapter-react";
+import {useEffect, useState} from "react";
+import {LAMPORTS_PER_SOL} from "@solana/web3.js";
+
 export const Balance = () => {
+  const [balance, setBalance] = useState<number>(0);
+  const {connection} = useConnection();
+  const {publicKey} = useWallet();
+
+  useEffect(() => {
+    if (!connection || !publicKey) return;
+    connection.getBalance(publicKey).then(setBalance);
+  }, [connection, publicKey]);
+
   return (
     <div className="mt-8">
       <div className="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
-        <div className="flex flex-col space-y-1.5 p-6">
+        <div className="flex flex-col items-center space-y-1.5 p-4">
           <h3 className="text-2xl font-semibold leading-none tracking-tight">Your SOL Balance</h3>
+          <span>◎{balance / LAMPORTS_PER_SOL}</span>
         </div>
-        <div className="p-6"></div>
       </div>
     </div>
   );
--- src/balance/BalancePage.tsx
+++ src/balance/BalancePage.tsx
@@ -1,17 +1,19 @@
 import {Balance} from "./Balance.tsx";
 import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import {useWallet} from "@solana/wallet-adapter-react";
 
 export const BalancePage = () => {
+  const {connected} = useWallet();
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
       <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Connect your Solana Wallet</h2>
-      <p className="mt-2 text-lg text-gray-600">
-        Click the button below to connect your Solana Wallet and view your SOL balance on devnet.
+      <p className="mt-1 text-lg text-center text-gray-600">
+        Click the button below to connect your Solana Wallet<br/>and view your SOL balance on devnet.
       </p>
       <div className="mt-8">
         <WalletMultiButton/>
       </div>
-      <Balance/>
+      {connected && <Balance/>}
     </main>
   );
 };
