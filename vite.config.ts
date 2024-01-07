df447123c40bb2225ff23a6f5ad051e5ea3cedd5
pabloaguilarm <paguilar@heytrade.com>
2024-01-07 23:07:04 +0100

    Fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["@particle-network/auth"]
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      zlib: 'browserify-zlib',
+      util: 'util',
+      http: 'stream-http',
+      https: 'https-browserify',
+      crypto: 'crypto-browserify',
+      os: 'os-browserify'
     }
-  }
-
+  },
+  plugins: [react()],
+  base: './'
 })
